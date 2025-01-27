89c4c7e22548b018a0bc0a124386c84b4fd5e010
harnwalN <neelaksh.harnwal@gmail.com>
2025-01-27 15:42:56 -0500

    Operator overloading for priority queue

--- src/Fundamentals/Sorting.cpp
+++ src/Fundamentals/Sorting.cpp
@@ -18,10 +18,14 @@ bool cmp2 (Number a, Number b) {
     // Custom comparator for custom Number class that sorts in ascending order based on "secondary_value"
     return a.secondary_value < b.secondary_value;
 }
+bool operator<(const Number &a, const Number &b) {
+    return a.secondary_value < b.secondary_value;
+}
 
 vector<int> v {1, 3, 2, 4, 5, 7, 6, 8};
 int arr[8] {1, 3, 2, 4, 5, 7, 6, 8};
 vector<Number> v_num = {Number(1, 8), Number(2, 7), Number(3, 6), Number(4, 5), Number(5, 4), Number(6, 3), Number(7, 2), Number(8, 1)};
+priority_queue<Number> pq;
 
 int n = 8;
 
@@ -29,6 +33,11 @@ void reset() {
     v = {1, 3, 2, 4, 5, 7, 6, 8};
     copy(v.begin(), v.end(), arr);
     v_num = {Number(1, 8), Number(2, 7), Number(3, 6), Number(4, 5), Number(5, 4), Number(6, 3), Number(7, 2), Number(8, 1)};
+
+    while (!pq.empty()) {pq.pop();}
+    for (int i = 1; i<=8; i++) {
+        pq.push(Number(i, 9-i));
+    }
 }
 
 void print() {
@@ -37,6 +46,14 @@ void print() {
     cout << "\nArray: ";
     for (int el : arr) cout << el;
     cout << "\n" << endl;
+}
+
+void print_pq() {
+    while (!pq.empty()) {
+        cout << "(" << pq.top().value << ", " << pq.top().secondary_value << ") ";
+        pq.pop();
+    }
+    cout << endl;
 }
 
 int main() {
@@ -73,4 +90,9 @@ int main() {
     sort(v_num.begin(), v_num.end(), cmp2);
     for (Number el : v_num) cout << "(" << el.value << ", " << el.secondary_value << ") ";
     cout << endl;
+
+    // Priority Queue Sorted based on overloaded "<" operator
+    cout << "Sorted priority queue based on overloaded '<' operator" << "\n";
+    print_pq();
+    reset();
 }
